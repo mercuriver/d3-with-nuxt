5f436b66e7628db9e14d2bff97727fd54b2a8df3
mercuriver <mercuriver@gmail.com>
2023-01-24 11:32:58 +0900

    feat: create chater 1 sample

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
   <h1>Hello world!</h1>
+  <div id="d3_layout"></div>
 </template>
+
+<script>
+import * as d3 from "d3";
+
+export default {
+  created() {
+    console.log(d3);
+  },
+};
+</script>
+
+<style scoped>
+#d3_layout {
+  background-color: aliceblue;
+  border: 1px solid salmon;
+}
+</style>
